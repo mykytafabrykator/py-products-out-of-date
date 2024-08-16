c93c7473ab7b3e5de706a462ed4e9d8d6859dc74
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-16 16:41:49 +0300

    Implement solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+import datetime
+from unittest import mock
+
+import pytest
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize(
+    "products,expected_result",
+    [
+        (
+            [{"name": "salmon",
+              "expiration_date": datetime.date(2022, 2, 10)}],
+            []
+        ),
+        (
+            [{"name": "salmon",
+              "expiration_date": datetime.date(2022, 2, 2)}],
+            ["salmon"]
+        )
+    ],
+    ids=[
+        "Valid product",
+        "Outdated product"
+    ]
+)
+@mock.patch("app.main.datetime")
+def test_outdated_products(mocked_date: mock,
+                           products: list,
+                           expected_result: list) -> None:
+    mocked_date.date.today.return_value = datetime.date(2022, 2, 5)
+    assert outdated_products(products) == expected_result
